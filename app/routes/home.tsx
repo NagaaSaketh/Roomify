e0eb4e51591b109731fd924e6d64316682f70d1c
Saketh <vnaagsaketh8925@gmail.com>
2026-02-16 18:36:41 +0530

    feat:added auth functionality using puter

---
+++ app/routes/home.tsx
@@ -0,0 +1,18 @@
+import type { Route } from "./+types/home";
+import NavBar from "../../components/NavBar";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <div className="home">
+      <NavBar/>
+      <h1 className="text-3xl text-indigo-300 font-extrabold">Hello there!</h1>
+    </div>
+  )
+}
---
+++ components/NavBar.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Box } from "lucide-react";
+import { useOutletContext } from "react-router";
+const NavBar = () => {
+  const { isSignedIn, userName, signIn, signOut } = useOutletContext();
+  const handleAuthClick = async () => {
+    if (isSignedIn) {
+      try {
+        await signOut();
+      } catch (err) {
+        console.error("Sign out failed!", err);
+      }
+
+      return;
+    }
+    try {
+      await signIn();
+    } catch (err) {
+      console.error("Sign in failed!", err);
+    }
+  };
+  return (
+    <header className="navbar">
+      <nav className="inner">
+        <div className="left">
+          <div className="brand">
+            <Box className="logo" />
+            <span className="name">Roomify</span>
+          </div>
+          <ul className="links">
+            <a href="#">Product</a>
+            <a href="#">Pricing</a>
+            <a href="#">Community</a>
+            <a href="#">Enterprise</a>
+          </ul>
+        </div>
+        <div className="actions">
+          {isSignedIn ? (
+            <>
+              <span className="greeting">
+                {userName ? `Hi, ${userName}` : `Signed in`}
+              </span>
+              <button className="login" onClick={handleAuthClick}>
+                Logout
+              </button>
+            </>
+          ) : (
+            <>
+              <button className="login" onClick={handleAuthClick}>
+                Login
+              </button>
+              <a className="cta" href="#upload">
+                Get Started
+              </a>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
